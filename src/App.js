547f6fe5d0ff673f8c05b7f6e705742490b16a10
Sana Khateeb <sana.khateeb@slalom.com>
2019-04-01 20:36:04 -0500

    Basic routing set up

--- src/App.js
+++ src/App.js
@@ -5,13 +5,26 @@ import Home from './pages/HomePage'
 import About from './pages/AboutPage'
 import Products from './pages/ProductsPage'
 import SingleProduct from './pages/SingleProductPage'
+import Cart from './pages/CartPage'
 import Contact from './pages/ContactPage'
 import Default from './pages/DefaultPage'
+import {Route, Switch} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <h1>Hellooooooo from tech store</h1>
+      <React.Fragment>
+        {/* navbar, sidebar, cart, footer */}
+        <Switch>
+          <Route path = "/" exact component={Home} />
+          <Route path = "/about" component={About} />
+          <Route path = "/contact" component={Contact} />
+          <Route path = "/products" exact component={Products} />
+          <Route path = "/products/:id" exact component={SingleProduct} />
+          <Route path = "/cart" component={Cart} />
+          <Route component={Default} />
+        </Switch>
+      </React.Fragment>
     );
   }
 }
