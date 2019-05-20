c2fd23fbda24850de8ed33944fba3c64fe3556fb
Sana Khateeb <sana.khateeb@slalom.com>
2019-05-20 16:03:06 -0600

    Set up links for navbar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import styled from 'styled-components'
+import {Link} from 'react-router-dom'
+import {ProductConsumer} from '../context'
 
 export default function Sidebar() {
   return (
-    <div>
-      Hello from Sidebar
-    </div>
+    <ProductConsumer>
+      {value => {
+        const {links, sidebarOpen, handleSidebar} = value;
+        return (
+          <SideWrapper>
+            <ul>
+              {links.map(link => {
+                return (
+                  <li key={link.id}>
+                    <Link to={link.path} className="sidebar-link" onClick={handleSidebar}>{link.text}</Link>
+                  </li>
+                )
+              })}
+            </ul>
+          </SideWrapper>
+        )
+      }}
+    </ProductConsumer>
   )
 }
+
+const SideWrapper = styled.nav`
+`
