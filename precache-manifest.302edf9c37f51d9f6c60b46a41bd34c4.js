bee193984a261f7234e4071a98e3d2c4cbda6eac
Sana Khateeb <sana.khateeb@slalom.com>
2019-05-23 20:02:30 -0600

    Updates

---
+++ precache-manifest.302edf9c37f51d9f6c60b46a41bd34c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b362308d9a07ef7d73f4865cbd132ab",
+    "url": "/tech-store/static/media/logo.2b362308.svg"
+  },
+  {
+    "revision": "b177d4b7b489e0dad5aa",
+    "url": "/tech-store/static/js/runtime~main.7dc53529.js"
+  },
+  {
+    "revision": "9589a6000d07fb9cb9ee",
+    "url": "/tech-store/static/js/main.4320d508.chunk.js"
+  },
+  {
+    "revision": "b83154555f6d0549bba2",
+    "url": "/tech-store/static/js/2.96dab73a.chunk.js"
+  },
+  {
+    "revision": "9589a6000d07fb9cb9ee",
+    "url": "/tech-store/static/css/main.76a17df5.chunk.css"
+  },
+  {
+    "revision": "b83154555f6d0549bba2",
+    "url": "/tech-store/static/css/2.8e9a5bdf.chunk.css"
+  },
+  {
+    "revision": "7801a96491e677316c0d4c00dcf1ab5c",
+    "url": "/tech-store/index.html"
+  }
+];
