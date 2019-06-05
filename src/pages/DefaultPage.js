7e7c92c297b07547437221acc1416dfd4bd17f99
Sana Khateeb <sana.khateeb@slalom.com>
2019-06-05 16:40:36 -0500

    Set up Default page

--- src/pages/DefaultPage.js
+++ src/pages/DefaultPage.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import Hero from '../components/Hero'
+import defaultBcg from '../images/defaultBcg.jpeg'
+import {Link} from 'react-router-dom'
 
 export default function DefaultPage() {
   return (
     <React.Fragment>
-      <h1>Hello from Default Page</h1>
+      <Hero img={defaultBcg} title="Error 404" max="true">
+        <h2 className="text-uppercase">
+          This is not the page you're looking for
+        </h2>
+        <Link to="/" className="main-link" style={{marginTop: "2rem"}}>
+          Return Home
+        </Link>
+      </Hero>
     </React.Fragment>
   )
 }
