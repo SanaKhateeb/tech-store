c233b08b0efdaa475a207d09276b94621db2ac29
Sana Khateeb <sana.khateeb@slalom.com>
2019-07-02 14:54:48 -0500

    Finished About Page

--- src/components/AboutPage/Info.js
+++ src/components/AboutPage/Info.js
@@ -1,13 +1,26 @@
 import React from 'react'
 import Title from '../Title'
+import aboutBcg from  '../../images/aboutBcg.jpeg'
 
 export default function Info() {
   return (
-    <div>
-      Hello from Info
-      <Title title="About Us">
+    <section className="py-5">
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto col-md-6 my-3">
+            <img src={aboutBcg} className="img-fluid img-thumbnail" alt= "about company" style={{background: "var(--darkGrey)"}}/>
+          </div>
 
-      </Title>
-    </div>
+          <div className="col-10 mx-auto col-md-6 my-3">
+            <Title title="About Us" />
+            <p className="text-lead text-muted my-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores molestias natus tempora ratione possimus nam praesentium, consequuntur neque modi ducimus?</p>
+            <p className="text-lead text-muted my-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores molestias natus tempora ratione possimus nam praesentium, consequuntur neque modi ducimus?</p>
+            <button className="main-link" type="button" style={{marginTop: "2rem"}}>
+              More Info
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
--- src/components/Title.js
+++ src/components/Title.js
@@ -15,5 +15,12 @@ export default function Title({title, center}) {
 }
 
 const TitleWrapper = styled.div`
-
+  text-align: ${props => props.center ? "center": "left"};
+  
+  .title-underline {
+    height: 0.25rem;
+    width: 7rem;
+    background: var(--primaryColor);
+    margin: ${props => props.center ? "0 auto": "0"};
+  }
 `
