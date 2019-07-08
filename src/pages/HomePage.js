a2a3bd3f9ad2ccd3018089fa671db41b49729f49
Sana Khateeb <sana.khateeb@slalom.com>
2019-07-08 12:47:02 -0500

    Added Services section on Homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,13 +2,19 @@ import React from 'react'
 import {ProductConsumer} from '../context';
 import Hero from '../components/Hero.js';
 import {Link} from 'react-router-dom';
+import Services from '../components/HomePage/Services'
+import Featured from '../components/HomePage/Featured'
 
 export default function HomePage() {
   return (
-    <Hero title="Awesome Gadgets" max="true">
-      <Link className="main-link" style={{margin: "2rem"}} to='/products'>
-        Our Products
-      </Link>
-    </Hero>
+    <>
+      <Hero title="Awesome Gadgets" max="true">
+        <Link className="main-link" style={{margin: "2rem"}} to='/products'>
+          Our Products
+        </Link>
+      </Hero>
+      <Services />
+      <Featured />
+    </>
   )
 }
