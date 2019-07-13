86966ce953e70c305c9933f9078f649e4f6986a0
Sana Khateeb <sana.khateeb@slalom.com>
2019-07-13 17:49:48 -0500

    Updates

---
+++ precache-manifest.cf7dab94ddac4c0dc26939d27d4f6f12.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc5579d09922568943c7436bf3388659",
+    "url": "/tech-store/index.html"
+  },
+  {
+    "revision": "6da4c9b1f421a4236ac4",
+    "url": "/tech-store/static/css/2.1e66eaca.chunk.css"
+  },
+  {
+    "revision": "76b58a8792ed783d6030",
+    "url": "/tech-store/static/css/main.d486e271.chunk.css"
+  },
+  {
+    "revision": "6da4c9b1f421a4236ac4",
+    "url": "/tech-store/static/js/2.0a9cab83.chunk.js"
+  },
+  {
+    "revision": "76b58a8792ed783d6030",
+    "url": "/tech-store/static/js/main.86aadd03.chunk.js"
+  },
+  {
+    "revision": "b177d4b7b489e0dad5aa",
+    "url": "/tech-store/static/js/runtime~main.7dc53529.js"
+  },
+  {
+    "revision": "7709c10e8c30753537d94a6b69d7542a",
+    "url": "/tech-store/static/media/aboutBcg.7709c10e.jpeg"
+  },
+  {
+    "revision": "945afb997e0731b038ed58fadacfeb3f",
+    "url": "/tech-store/static/media/contactBcg.945afb99.jpeg"
+  },
+  {
+    "revision": "0819a81634391f8518fe325378b8f994",
+    "url": "/tech-store/static/media/defaultBcg.0819a816.jpeg"
+  },
+  {
+    "revision": "2b362308d9a07ef7d73f4865cbd132ab",
+    "url": "/tech-store/static/media/logo.2b362308.svg"
+  },
+  {
+    "revision": "984874d19d1cdef8dcee5fcea55c082d",
+    "url": "/tech-store/static/media/mainBcg.984874d1.jpeg"
+  },
+  {
+    "revision": "160fddff3ca71887ebf8da3929244379",
+    "url": "/tech-store/static/media/productsBcg.160fddff.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tech-store/precache-manifest.c32aba7b0ae19b6e6c3d882a49f569e0.js"
+  "/tech-store/precache-manifest.cf7dab94ddac4c0dc26939d27d4f6f12.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/tech-store/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tech-store/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
