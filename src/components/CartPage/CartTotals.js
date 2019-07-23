6d7b5bfb45dfad80fc21d0142c710bcd920279a7
Sana Khateeb <sana.khateeb@slalom.com>
2019-07-23 10:34:06 -0600

    Finished cart totals subcomponent

--- src/components/CartPage/CartTotals.js
+++ src/components/CartPage/CartTotals.js
@@ -1,9 +1,33 @@
 import React from 'react'
+import {ProductConsumer} from '../../context'
 
 export default function CartTotals() {
   return (
-    <div>
-      CartTotels
+    <div className="container">
+      <div className="row">
+        <ProductConsumer>
+          {value => {
+            const {clearCart, cartSubTotal, cartTax, cartTotal} = value;
+
+            return (
+              <div className="col text-title text-center my-4">
+                <button className="btn btn-outline-danger text-uppercase mb-4" onClick={clearCart}>
+                  Clear Cart
+                </button>
+                <h3>
+                  Subtotal: ${cartSubTotal}
+                </h3>
+                <h3>
+                  Tax: ${cartTax}
+                </h3>
+                <h3>
+                  Total: ${cartTotal}
+                </h3>
+              </div>
+            )
+          }}
+        </ProductConsumer>
+      </div>
     </div>
   )
 }
